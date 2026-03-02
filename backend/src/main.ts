3dd627d52f9a4bdc6b526538d80e90895f08762a
Lucas Alexandre Davila <ldev@MacBook-Air-de-Lucas.local>
2026-03-01 22:22:22 -0300

    refactor(core): remove examples from main entrypoint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,50 +1,15 @@
 import { Winter, WinterWelcome } from './@winterFramework/app/winter.app'
 
-// --- DI Example ---
-import { UserService } from './examples/user.service'
-import { UserController } from './examples/user.controller'
-
-// --- Exception Handling Example ---
-import { GlobalExceptionHandler } from './examples/global-exception-handler'
-
-// --- Interceptor Examples ---
-import { LoggingInterceptor } from './examples/logging.interceptor'
-import { InterceptorExampleController } from './examples/interceptor-example.controller'
-
-// --- Parameter Injection Example ---
-import { ParamExampleController } from './examples/params-example.controller'
-
-// --- Validation Example ---
-import { ValidationExampleController } from './examples/validation-example.controller'
-
-// --- Middleware Example ---
-import { MiddlewareExampleController } from './examples/middleware-example.controller'
-
-// --- DebuggerLogger Example ---
-import { DebugExampleController } from './examples/debugger-example.controller'
-
 Winter.create()
   .setName('WinterFramework') // Project Name
   .setEnv('dev') // dev = 1337 | prod = 8080
-
-  // DI Providers
-  .addProvider(UserService)
-
-  // Controllers
-  .addController(
-    WinterWelcome,
-    UserController,
-    ParamExampleController,
-    ValidationExampleController,
-    MiddlewareExampleController,
-    DebugExampleController,
-    InterceptorExampleController,
-  )
-
-  // Exception handling
-  .addControllerAdvice(GlobalExceptionHandler)
-
-  // Global interceptors
-  .addInterceptor(LoggingInterceptor)
-
+  // .setPort(3000) // Custom Port
+  .addController(WinterWelcome)
+  .addMiddleware((app) => {
+    // Middlewares
+    app.use('*', async (ctx, next) => {
+      console.log(`${ctx.req.method} ${ctx.req.url}`)
+      await next()
+    })
+  })
   .start()
